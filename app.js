3b00936a046cd1750b8ac45b1757e80d93cd96a3
jjbskir <jjbskir@gmail.com>
2014-07-13 22:15:42 -0400

    removed

--- app.js
+++ app.js
@@ -117,4 +117,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
 module.exports = app;
